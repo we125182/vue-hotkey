e1ee5b8ade4d43545c2519a5f50c9574c6bf72ff
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2020-08-28 14:03:21 +0200

    Make debugging in browser devtools more pleasant

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,74 @@
-module.exports = {
+const common = {
     publicPath: './',
-    // runtimeCompiler: true,
+
+    configureWebpack: {
+        output: {
+            devtoolModuleFilenameTemplate,
+            devtoolFallbackModuleFilenameTemplate: 'webpack-fallback:///[resource-path]?[hash]',
+        },
+    },
+
+    chainWebpack: config => {
+        config.plugins.delete('friendly-errors')
+    },
+
+    productionSourceMap: false,
 }
+
+const configs = {
+    development: extend(common,
+        {
+            devtool: 'eval-source-map',
+            module: {
+                rules: [
+                    {
+                        use: ['source-map-loader'],
+                        test: /\.js$/,
+                        enforce: 'pre',
+                    },
+                ],
+            },
+        },
+        config => config.plugins.delete('prefetch')),
+
+    test: common,
+
+    production: extend(common,
+        {
+            optimization: {
+                splitChunks: {
+                    maxSize: 250000,
+                }
+            }
+        }),
+}
+
+
+function extend(common, configureWebpack = {}, chainWebpack = () => {}) {
+    return {
+        ...common,
+        configureWebpack: {
+            ...common.configureWebpack,
+            ...configureWebpack,
+        },
+        chainWebpack: config => {
+            common.chainWebpack(config)
+            chainWebpack(config)
+        },
+    }
+}
+
+// Preserve source file paths in browser devtools
+function devtoolModuleFilenameTemplate(info) {
+    if (info.resourcePath.match(/\.(vue|css)$/) && info.allLoaders) {
+        return `webpack:///${info.resourcePath}?${info.hash}`
+
+    } else {
+        // Make sure no file ends up outside 'sources://'
+        let here = info.resourcePath.startsWith('./') ? '' : './'
+        return `sources://${here}${info.resourcePath}`
+    }
+}
+
+
+module.exports = configs[process.env.NODE_ENV] || configs.production
